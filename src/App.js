448f16fbf5e5d4d59eb6d81cb367ea4908b9e1ba
Niraj Raut <140501578+nraut6090@users.noreply.github.com>
2023-08-26 23:29:16 +0530

    Update App.js home url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
     <BrowserRouter>
     <Routes>
-      <Route exact path="/home" element={<Home/>}/>
+      <Route exact path="/" element={<Home/>}/>
       <Route exact path="/about" element={<About/>}/>
     </Routes>
     </BrowserRouter>
